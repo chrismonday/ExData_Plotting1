8bc3fa55f971b06046d4a3d9c1d3d5736ec1da61
chris monday <chrismonday@gmail.com>
2021-06-13 18:28:33 +0900

    Here is the code from R studio and the 4 plots for HW1 on Coursera

--- code for hw 1.R
+++ code for hw 1.R
@@ -1,57 +1,67 @@
-t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
-
-## Format date to Type Date
-t$Date <- as.Date(t$Date, "%d/%m/%Y")
-
-## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
-t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
-
-## Remove incomplete observation
-t <- t[complete.cases(t),]
-
-## Combine Date and Time column
-dateTime <- paste(t$Date, t$Time)
-
-## Name the vector
-dateTime <- setNames(dateTime, "DateTime")
-
-## Remove Date and Time column
-t <- t[ ,!(names(t) %in% c("Date","Time"))]
-
-## Add DateTime column
-t <- cbind(dateTime, t)
-
-## Format dateTime Column
-t$dateTime <- as.POSIXct(dateTime)
-
-## Create the histogram
-hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
-## Create Plot 2
-plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
-#dev.copy(png,"plot2.png", width=480, height=480)
-#dev.off()
-## Create Plot 3
-with(t, {
-  plot(Sub_metering_1~dateTime, type="l",
-       ylab="Global Active Power (kilowatts)", xlab="")
-  lines(Sub_metering_2~dateTime,col='Red')
-  lines(Sub_metering_3~dateTime,col='Blue')
-})
-legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-## Create Plot 4
-par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-with(t, {
-  plot(Global_active_power~dateTime, type="l", 
-       ylab="Global Active Power (kilowatts)", xlab="")
-  plot(Voltage~dateTime, type="l", 
-       ylab="Voltage (volt)", xlab="")
-  plot(Sub_metering_1~dateTime, type="l", 
-       ylab="Global Active Power (kilowatts)", xlab="")
-  lines(Sub_metering_2~dateTime,col='Red')
-  lines(Sub_metering_3~dateTime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(Global_reactive_power~dateTime, type="l", 
-       ylab="Global Rective Power (kilowatts)",xlab="")
-})
+fileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl,destfile=paste0(getwd(),"/Power_consumption.zip"),method = "curl")
+unzip("Power_consumption.zip")
+initial<-read.table("household_power_consumption.txt", header=TRUE,sep=";", nrows=5)
+hpc<-read.table("household_power_consumption.txt", header=TRUE,sep=";", skip=66630, 
+                nrows=2900, col.names=names(initial), na.strings=c("?"),
+                colClasses=c("character", "character","numeric","numeric","numeric","numeric",
+                             "numeric","numeric","numeric"))
+hpc$Date<-as.Date(hpc$Date, format = "%d/%m/%Y")
+hpc$Time<-strptime(paste(hpc$Date,hpc$Time),"%F %T")
+hpc<-subset(hpc,hpc$Date %in% as.Date(c("2007-02-01","2007-02-02")))
+par(mfrow=c(1,1),mar=c(5,4.5,4,2))
+hist(hpc$Global_active_power, col="red", main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
+png("plot1.png", width=480, height=480)
+par(mfrow=c(1,1),mar=c(5,4.5,4,2))
+hist(hpc$Global_active_power, col="red", main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
+dev.off()
+par(mfrow=c(1,1),mar=c(5,4.5,4,2))
+plot(hpc$Time,hpc$Global_active_power, ylab="Global Active Power (kilowatts)", 
+     xlab="", pch =".", type="l")
+png("plot2.png", width=480, height=480)
+par(mfrow=c(1,1),mar=c(5,4.5,4,2))
+plot(hpc$Time,hpc$Global_active_power, ylab="Global Active Power (kilowatts)", 
+     xlab="", pch =".", type="l")
+dev.off()
+par(mfrow=c(1,1),mar=c(4,4,4,2))
+plot(hpc$Time,hpc$Sub_metering_1,ylab="Energy sub metering", xlab="", type="l", col="black")
+points(hpc$Time,hpc$Sub_metering_2, col="red", type="l")
+points(hpc$Time,hpc$Sub_metering_3, col="blue", type="l")
+legend("topright", lwd=1, col=c("black", "red", "blue"), legend=names(hpc[,7:9]))
+png("plot3.png", width=480, height=480)
+par(mfrow=c(1,1),mar=c(4,4,4,2))
+plot(hpc$Time,hpc$Sub_metering_1,ylab="Energy sub metering", xlab="", type="l", col="black")
+points(hpc$Time,hpc$Sub_metering_2, col="red", type="l")
+points(hpc$Time,hpc$Sub_metering_3, col="blue", type="l")
+legend("topright", lwd=1, col=c("black", "red", "blue"), legend=names(hpc[,7:9]))
+dev.off()
+par(mfcol=c(2,2), mar=c(4.5,5,2,2))
+# Plot 4.1
+plot(hpc$Time,hpc$Global_active_power, ylab="Global Active Power (kilowatts)", 
+     xlab="", pch =".", type="l")
+# Plot 4.2
+plot(hpc$Time,hpc$Sub_metering_1,ylab="Energy sub metering", xlab="", type="l", col="black")
+points(hpc$Time,hpc$Sub_metering_2, col="red", type="l")
+points(hpc$Time,hpc$Sub_metering_3, col="blue", type="l")
+legend("topright", lwd=1, col=c("black", "red", "blue"), legend=names(hpc[,7:9]), bty="n")
+# Plot 4.3
+plot(hpc$Time,hpc$Voltage, ylab="Voltage", xlab="datetime", type="l")
+# Plot 4.4
+plot(hpc$Time,hpc$Global_reactive_power, ylab="Global_reactive_power", xlab="datetime", type="l")
+png("plot4.png", width=480, height=480)
+par(mfcol=c(2,2), mar=c(4.5,5,2,2))
+# Plot 4.1
+plot(hpc$Time,hpc$Global_active_power, ylab="Global Active Power (kilowatts)", 
+     xlab="", pch =".", type="l")
+# Plot 4.2
+plot(hpc$Time,hpc$Sub_metering_1,ylab="Energy sub metering", xlab="", type="l", col="black")
+points(hpc$Time,hpc$Sub_metering_2, col="red", type="l")
+points(hpc$Time,hpc$Sub_metering_3, col="blue", type="l")
+legend("topright", lwd=1, col=c("black", "red", "blue"), legend=names(hpc[,7:9]), bty="n")
+# Plot 4.3
+plot(hpc$Time,hpc$Voltage, ylab="Voltage", xlab="datetime", type="l")
+# Plot 4.4
+plot(hpc$Time,hpc$Global_reactive_power, ylab="Global_reactive_power", xlab="datetime", type="l")
+dev.off()
